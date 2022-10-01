1f9c155f38dca0b7114d91ee0db80ec3c12b2a74
Ø <fncolon@pm.me>
2022-10-01 22:21:14 +0700

    test!: prepare to move to operator units test

--- test/integration/operators.test.js
+++ test/integration/operators.test.js
@@ -39,44 +39,6 @@ describe(Support.getTestDialectTeaser('Operators'), () => {
           type: DataTypes.STRING,
         },
       });
-
-      if (dialect === 'postgres') {
-        this.UserPg = this.sequelize.define('userPg', {
-          id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-            field: 'userId',
-          },
-          name: {
-            type: DataTypes.STRING,
-            field: 'full_name',
-          },
-          metadata: {
-            type: DataTypes.JSONB,
-            field: 'metadata',
-          },
-        }, {
-          tableName: 'users_pg',
-          timestamps: false,
-        });
-
-        await this.sequelize.getQueryInterface().createTable('users_pg', {
-          userId: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-          },
-          full_name: {
-            type: DataTypes.STRING,
-          },
-          metadata: {
-            type: DataTypes.JSONB,
-          },
-        });
-      }
     });
 
     if (['mysql', 'postgres'].includes(dialect)) {
@@ -119,33 +81,10 @@ describe(Support.getTestDialectTeaser('Operators'), () => {
     }
 
     if (dialect === 'postgres') {
-      describe('top level keys', () => {
-        it('should properly find any of array strings exist as top-level keys', async function () {
-          await this.UserPg.create({ name: 'Foobar', metadata: JSON.stringify({ threat: 'unclassified', teacherId: 1, studentId: 2 }) });
-          const user = await this.UserPg.findOne({
-            where: {
-              name: { [Op.anyKeyExists]: ['studentId'] },
-            },
-          });
-          // TODO: modify this instead of ok
-          expect(user).to.be.ok;
-        });
-        it('should properly find all of array strings exist as top-level keys', async function () {
-          await this.UserPg.create({ name: 'Foobar', metadata: JSON.stringify({ threat: 'unclassified', teacherId: 1, studentId: 2 }) });
-          const user = await this.UserPg.findOne({
-            where: {
-              name: { [Op.allKeysExist]: ['studentId', 'teacherId'] },
-            },
-          });
-          // TODO: modify this instead of ok
-          expect(user).to.be.ok;
-        });
-      });
-
       describe('case insensitive', () => {
         it('should work with a case-insensitive regexp where', async function () {
-          await this.UserPg.create({ name: 'Foobar' });
-          const user = await this.UserPg.findOne({
+          await this.User.create({ name: 'Foobar' });
+          const user = await this.User.findOne({
             where: {
               name: { [Op.iRegexp]: '^foo' },
             },
@@ -154,8 +93,8 @@ describe(Support.getTestDialectTeaser('Operators'), () => {
         });
 
         it('should work with a case-insensitive not regexp where', async function () {
-          await this.UserPg.create({ name: 'Foobar' });
-          const user = await this.UserPg.findOne({
+          await this.User.create({ name: 'Foobar' });
+          const user = await this.User.findOne({
             where: {
               name: { [Op.notIRegexp]: '^foo' },
             },
@@ -164,18 +103,18 @@ describe(Support.getTestDialectTeaser('Operators'), () => {
         });
 
         it('should properly escape regular expressions', async function () {
-          await this.UserPg.bulkCreate([{ name: 'John' }, { name: 'Bob' }]);
-          await this.UserPg.findAll({
+          await this.User.bulkCreate([{ name: 'John' }, { name: 'Bob' }]);
+          await this.User.findAll({
             where: {
               name: { [Op.iRegexp]: 'Bob\'; drop table users --' },
             },
           });
-          await this.UserPg.findAll({
+          await this.User.findAll({
             where: {
               name: { [Op.notIRegexp]: 'Bob\'; drop table users --' },
             },
           });
-          expect(await this.UserPg.findAll()).to.have.length(2);
+          expect(await this.User.findAll()).to.have.length(2);
         });
       });
     }
